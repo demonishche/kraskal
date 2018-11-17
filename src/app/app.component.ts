79c76ece6caf69c93e9ab019e0f0c308b4f42b8c
DESKTOP-POUBFF4\dimon <dimon_bc95@outlook.com>
2018-11-17 16:03:41 +0200

    ddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as d3 from 'd3';
+declare let d3: any;
 
 export interface Edge {
 	from: number,
@@ -39,6 +41,55 @@ export class AppComponent {
   constructor() {
 
   }
+
+  buildGraph() {
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", 600)
+		.attr("height", 600)
+		.append("g");	
+
+	var force = d3.layout.force()
+		.gravity(.05)
+		.distance(100)
+		.charge(-100)
+		.size([600, 600]);
+
+	force
+		.nodes(this.vertexes)
+		.links(this.edges)
+		.start();
+  
+	var link = svg.selectAll(".link")
+		.data(this.edges)
+	  .enter().append("line")
+		.attr("class", "link")
+	//   .style("stroke-width", function(d) {  return Math.sqrt(d.weight); });
+  
+	var node = svg.selectAll(".node")
+		.data(this.vertexes)
+	  .enter().append("g")
+		.attr("class", "node")
+		.call(force.drag);
+  
+	node.append("circle")
+		.attr("r","5");
+  
+	node.append("text")
+		.attr("dx", 12)
+		.attr("dy", ".35em")
+		.text(function(d) { return d });
+  
+	force.on("tick", function() {
+	  link.attr("x1", function(d) { return d.from.x; })
+		  .attr("y1", function(d) { return d.from.y; })
+		  .attr("x2", function(d) { return d.to.x; })
+		  .attr("y2", function(d) { return d.to.y; });
+  
+	  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	});
+  }
+  
 
   addVertex() {
   	this.vertexes.push(this.counter);
